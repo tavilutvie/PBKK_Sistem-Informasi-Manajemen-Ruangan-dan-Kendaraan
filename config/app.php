18f21c8d8c05b4f68e69680173ed1e151d4f9442
Eldenabih Tavirazin Lutvie <tavirazin@gmail.com>
2023-06-20 20:10:55 +0700

    deploy web

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://pbkk-sistem-informasi-manajemen-ruangan-dan-kendaraan.vercel.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
